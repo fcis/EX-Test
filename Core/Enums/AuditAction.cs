30a966dba49cba15bba9974872a7c938a20c0732
Mohamed Nabarawy <m.gamal.nabarawy@gmail.com>
2025-03-18 15:59:10 +0200

    s

---
+++ Core/Enums/AuditAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Enums
+{
+    public enum AuditAction
+    {
+        ADD,
+        EDIT,
+        DELETE,
+        LOGON,
+        LOGOUT
+    }
+}
---
+++ Core/Enums/OrganizationMembershipStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Enums
+{
+    public enum OrganizationMembershipStatus
+    {
+        ACTIVE,
+        SUSPENDED,
+        COMPLETED,
+        EXPIRED,
+        DELETED
+    }
+}
