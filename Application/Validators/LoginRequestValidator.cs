59ef87ebf4420725a23674392f5cc716511335b5
Mohamed Nabarawy <m.gamal.nabarawy@gmail.com>
2025-03-22 20:28:02 +0200

    login request validator

--- Application/Validators/LoginRequestValidator.cs
+++ Application/Validators/LoginRequestValidator.cs
@@ -16,7 +16,13 @@ namespace Application.Validators
                 .NotEmpty().WithMessage("Username is required");
 
             RuleFor(x => x.Password)
-                .NotEmpty().WithMessage("Password is required");
+                .NotEmpty().WithMessage("Password is required")
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters")
+                .MaximumLength(100).WithMessage("Password cannot exceed 100 characters")
+                .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter")
+                .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter")
+                .Matches("[0-9]").WithMessage("Password must contain at least one number")
+                .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character");
         }
     }
 }
